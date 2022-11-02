04898e16dc62ef1dfe45a5c1b5101bd8acb7db0e
张凯 <804703017@qq.com>
2022-11-02 16:26:19 +0800

    处理http全局异常处理器返回中文乱码：复制到记事本再粘贴进去

--- mall4cloud-common/mall4cloud-common-core/src/main/java/com/mall4j/cloud/common/response/ResponseEnum.java
+++ mall4cloud-common/mall4cloud-common-core/src/main/java/com/mall4j/cloud/common/response/ResponseEnum.java
@@ -140,7 +140,7 @@ public enum ResponseEnum {
 	 * 账号未注册，前端看到这个状态码，弹出选择框，提示用户账号未注册，是否进入注册页面，用户选择是，进入注册页面
 	 */
 	ACCOUNT_NOT_REGISTER("A04003", "account not register");
-    private final String code;
+	private final String code;
 
 	private final String msg;
 
